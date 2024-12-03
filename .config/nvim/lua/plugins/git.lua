61a9de513d43669d074de70dacfa9f4f8e10fdad
raphaelmso <raphael.msoliveira@gmail.com>
2024-12-02 23:01:38 -0300

    remove fugitive

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,11 +1,5 @@
 return {
 	{
-		"tpope/vim-fugitive",
-		config = function()
-			vim.keymap.set("n", "<leader>g", ":G ")
-		end,
-	},
-	{
 		"lewis6991/gitsigns.nvim",
 		config = function()
 			require("gitsigns").setup()
