16333a70822bb605f86e71d0958a46561c2776aa
raphaelmso <raphael.msoliveira@gmail.com>
2024-12-16 15:31:52 -0300

    remove border

--- .config/nvim/lua/plugins/notify.lua
+++ .config/nvim/lua/plugins/notify.lua
@@ -6,7 +6,13 @@ return {
 			render = "compact",
 			stages = "static",
 			border = "none",
+			on_open = function(win)
+				local config = vim.api.nvim_win_get_config(win)
+				config.border = "none"
+				vim.api.nvim_win_set_config(win, config)
+			end,
 		})
 		vim.notify = notify
 	end,
+
 }
