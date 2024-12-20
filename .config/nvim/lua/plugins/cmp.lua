2970b38dabb3d64857c3ffbe487720c8f99ffa44
raphaelmso <raphael.msoliveira@gmail.com>
2024-12-20 14:22:58 -0300

    update to 0.8.0

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,55 +1,36 @@
 return {
-	{
-		"saghen/blink.cmp",
-		lazy = false,
-		dependencies = {
-			"rafamadriz/friendly-snippets",
+	"saghen/blink.cmp",
+	dependencies = "rafamadriz/friendly-snippets",
+	version = "v0.*",
+
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+    ---
+	opts = {
+		keymap = {
+			preset = "default",
+
+			["<CR>"] = { "select_and_accept", "fallback" },
+			["<Tab>"] = { "select_next", "fallback" },
+			["<S-Tab>"] = { "select_prev", "fallback" },
+			["<C-j>"] = { "select_next", "fallback" },
+			["<C-k>"] = { "select_prev", "fallback" },
+			["<C-n>"] = { "scroll_documentation_down" },
+			["<C-p>"] = { "scroll_documentation_up" },
+
+			cmdline = {
+				preset = "super-tab",
+			},
 		},
 
-		version = "v0.*",
+		appearance = {
+			use_nvim_cmp_as_default = true,
+			nerd_font_variant = "mono",
+		},
 
-		---@module 'blink.cmp'
-		---@type blink.cmp.Config
-		opts = {
-			keymap = {
-				preset = "default",
-
-				["<CR>"] = { "select_and_accept", "fallback" },
-				["<Tab>"] = { "select_next", "fallback" },
-				["<S-Tab>"] = { "select_prev", "fallback" },
-				["<C-j>"] = { "select_next", "fallback" },
-				["<C-k>"] = { "select_prev", "fallback" },
-				["<C-n>"] = { "scroll_documentation_down" },
-				["<C-p>"] = { "scroll_documentation_up" },
-
-				cmdline = {
-					preset = "super-tab",
-				},
-			},
-
-			appearance = {
-				use_nvim_cmp_as_default = true,
-				nerd_font_variant = "mono",
-			},
-
-			snippets = {
-				name = "Snippets",
-				module = "blink.cmp.sources.snippets",
-				opts = {
-					friendly_snippets = true,
-					search_paths = { vim.fn.stdpath("config") .. "/snippets" },
-					global_snippets = { "all" },
-					extended_filetypes = {},
-					ignored_filetypes = {},
-					get_filetype = function(context)
-						return vim.bo.filetype
-					end,
-				},
-				sources = {
-					default = { "lsp", "path", "luasnip", "buffer" },
-				},
-			},
-			opts_extend = { "sources.default" },
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
 		},
 	},
+	opts_extend = { "sources.default" },
 }
