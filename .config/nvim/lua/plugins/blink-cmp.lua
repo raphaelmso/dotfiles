a36e28d558797f5a6c2f3b4b1052f2bcc507ead8
raphaelmso <raphael.msoliveira@gmail.com>
2024-12-21 13:43:22 -0300

    minor changes

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -1,33 +1,46 @@
 return {
-	"saghen/blink.cmp",
-	dependencies = "rafamadriz/friendly-snippets",
-	version = "v0.*",
+    "saghen/blink.cmp",
+    dependencies = "rafamadriz/friendly-snippets",
+    version = "v0.*",
 
-	opts = {
-		keymap = {
-			preset = "default",
+    opts = {
+        keymap = {
+            preset = "default",
 
-			["<CR>"] = { "select_and_accept", "fallback" },
-			["<Tab>"] = { "select_next", "fallback" },
-			["<S-Tab>"] = { "select_prev", "fallback" },
-			["<C-j>"] = { "select_next", "fallback" },
-			["<C-k>"] = { "select_prev", "fallback" },
-			["<C-n>"] = { "scroll_documentation_down" },
-			["<C-p>"] = { "scroll_documentation_up" },
+            ["<CR>"] = { "select_and_accept", "fallback" },
+            ["<Tab>"] = { "select_next", "fallback" },
+            ["<S-Tab>"] = { "select_prev", "fallback" },
+            ["<C-j>"] = { "select_next", "fallback" },
+            ["<C-k>"] = { "select_prev", "fallback" },
+            ["<C-n>"] = { "scroll_documentation_down" },
+            ["<C-p>"] = { "scroll_documentation_up" },
 
-			cmdline = {
-				preset = "super-tab",
-			},
-		},
+            cmdline = {
+                preset = "super-tab",
+            },
+        },
 
-		appearance = {
-			use_nvim_cmp_as_default = true,
-			nerd_font_variant = "mono",
-		},
+        appearance = {
+            use_nvim_cmp_as_default = false,
+            nerd_font_variant = "mono",
+            kind_icons = {
+                Method = '󰪍',
+                Snippet = '',
 
-		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
-		},
-	},
-	opts_extend = { "sources.default" },
+            },
+        },
+
+        sources = {
+            default = { "lsp", "path", "snippets", "buffer" },
+            providers = {
+                lazydev = {
+                    name = "LazyDev",
+                    module = "lazydev.integrations.blink",
+                    -- make lazydev completions top priority (see `:h blink.cmp`)
+                    score_offset = 100,
+                },
+            },
+        },
+    },
+    opts_extend = { "sources.default" },
 }
