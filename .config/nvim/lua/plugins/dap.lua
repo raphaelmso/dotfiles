259081191a4456998f20e62ede1276ac41c95f0d
raphaelmso <raphael.msoliveira@gmail.com>
2024-12-21 13:43:41 -0300

    remove unused keymaps

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -3,18 +3,6 @@ return {
         "mfussenegger/nvim-dap",
         config = function()
             local dap = require("dap")
-            -- vim.keymap.set(
-            -- 	"n",
-            -- 	"<leader>dB",
-            -- 	":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>"
-            -- )
-            -- vim.keymap.set("n", "<leader>db", ":lua require('dap').toggle_breakpoint()<cr>")
-            -- vim.keymap.set("n", "<F5>", ":lua require('dap').continue()<cr>")
-            -- vim.keymap.set("n", "<F10>", ":lua require('dap').step_over()<cr>")
-            -- vim.keymap.set("n", "<F11>", ":lua require('dap').step_into()<cr>")
-            -- vim.keymap.set("n", "<F12>", ":lua require('dap').step_out()<cr>")
-            -- vim.keymap.set("n", "<leader>dl", ":lua require('dap').run_last()<cr>")
-            -- vim.keymap.set("n", "<leader>dl", ":lua require('dap').run_last()<cr>")
         end,
     },
 
