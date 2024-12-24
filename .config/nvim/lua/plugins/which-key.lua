eb5dfc9a09d13b1d718e693fa179c74eb1f0ca1b
raphaelmso <raphael.msoliveira@gmail.com>
2024-12-24 10:30:37 -0300

    add git key group

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -23,6 +23,7 @@ return {
         vim.keymap.set("n", "<leader>c", "", { desc = "Code" })
         vim.keymap.set("n", "<leader>d", "", { desc = "Debug" })
         vim.keymap.set("n", "<leader>f", "", { desc = "Find" })
+        vim.keymap.set("n", "<leader>g", "", { desc = "Git" })
         vim.keymap.set("n", "<leader>s", "", { desc = "Split" })
         vim.keymap.set("n", "<leader>t", "", { desc = "Diagnostics" })
         vim.keymap.set("n", "<leader>x", "", { desc = "General" })
