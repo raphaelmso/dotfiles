e88eaba802ed4ce8c8f1afab6c3bd3a4d1b5afe7
raphaelmso <raphael.msoliveira@gmail.com>
2024-12-28 12:14:07 -0300

    change icons

--- .config/nvim/lua/plugins/core/blink-cmp.lua
+++ .config/nvim/lua/plugins/core/blink-cmp.lua
@@ -23,13 +23,53 @@ return {
             },
         },
 
+        completion = {
+            menu = {
+                draw = {
+                    treesitter = { "lsp" },
+                },
+            },
+            documentation = {
+                auto_show = true,
+                auto_show_delay_ms = 200,
+            },
+        },
+
+
         appearance = {
             use_nvim_cmp_as_default = false,
             nerd_font_variant = "mono",
             kind_icons = {
-                Method = '󰪍',
-                Snippet = '',
+                Text = '󱀍',
+                Method = '󰡱',
+                Function = '󰊕',
+                Constructor = '󰒓',
 
+                Field = '',
+                Variable = '',
+                Property = '󰖷',
+
+                Class = '',
+                Interface = '',
+                Struct = '',
+                Module = '󰅩',
+
+                Unit = '',
+                Value = '',
+                Enum = '',
+                EnumMember = '',
+
+                Keyword = '',
+                Constant = '󰏿',
+
+                Snippet = '',
+                Color = '󰏘',
+                File = '󰈔',
+                Reference = '',
+                Folder = '󰉋',
+                Event = '󱐋',
+                Operator = '',
+                TypeParameter = '',
             },
         },
 
