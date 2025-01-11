176789124f70298d853d45a9e0de0f2f1833ae75
raphaelmso <raphael.msoliveira@gmail.com>
2025-01-11 18:42:56 -0300

    remove format on save

--- .config/nvim/lua/plugins/core/lspconfig.lua
+++ .config/nvim/lua/plugins/core/lspconfig.lua
@@ -53,22 +53,22 @@ return {
                 }
             })
 
-            vim.api.nvim_create_autocmd('LspAttach', {
-                callback = function(args)
-                    local client = vim.lsp.get_client_by_id(args.data.client_id)
-                    if not client then return end
-
-                    if client:supports_method('textDocument/formatting') then
-                        -- Format the current buffer on save
-                        vim.api.nvim_create_autocmd('BufWritePre', {
-                            buffer = args.buf,
-                            callback = function()
-                                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-                            end,
-                        })
-                    end
-                end,
-            })
+            -- vim.api.nvim_create_autocmd('LspAttach', {
+            --     callback = function(args)
+            --         local client = vim.lsp.get_client_by_id(args.data.client_id)
+            --         if not client then return end
+            --
+            --         if client:supports_method('textDocument/formatting') then
+            --             -- Format the current buffer on save
+            --             vim.api.nvim_create_autocmd('BufWritePre', {
+            --                 buffer = args.buf,
+            --                 callback = function()
+            --                     vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+            --                 end,
+            --             })
+            --         end
+            --     end,
+            -- })
 
             vim.diagnostic.config {
                 virtual_text = false,
@@ -100,4 +100,3 @@ return {
         end,
     },
 }
-
