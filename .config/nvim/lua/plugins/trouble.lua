087d6dbb02f91989044120fe42760c5138c29102
raphaelmso <raphael.msoliveira@gmail.com>
2025-01-15 10:20:38 -0300

    add keymaps

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -10,27 +10,27 @@ return {
                 desc = "Diagnostics",
             },
             {
-                "<leader>tb",
+                "<leader>Xb",
                 "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
                 desc = "Buffer Diagnostics (Trouble)",
             },
             {
-                "<leader>ts",
+                "<leader>Xs",
                 "<cmd>Trouble symbols toggle focus=false<cr>",
                 desc = "Symbols (Trouble)",
             },
             {
-                "<leader>tl",
+                "<leader>Xl",
                 "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
                 desc = "LSP Definitions / references / ... (Trouble)",
             },
             {
-                "<leader>to",
+                "<leader>Xo",
                 "<cmd>Trouble loclist toggle<cr>",
                 desc = "Location List (Trouble)",
             },
             {
-                "<leader>tq",
+                "<leader>Xq",
                 "<cmd>Trouble qflist toggle<cr>",
                 desc = "Quickfix List (Trouble)",
             },
