68be79c62bc92f7001d5c1d51ab66ea04a3cb805
raphaelmso <raphael.msoliveira@gmail.com>
2025-01-23 18:28:11 -0300

    add bufferline

---
+++ .config/nvim/lua/autocmds.lua
@@ -0,0 +1,8 @@
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = "IncSearch",
+            timeout = 100,
+        })
+    end,
+})
---
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,25 @@
+return {
+    "akinsho/bufferline.nvim",
+    config = function()
+        require("bufferline").setup({
+            highlights = {
+                fill = {
+                    bg = "#16161e",
+                },
+                background = {
+                    bg = "#16161e",
+                },
+                buffer_selected = {
+                    bg = "#1a1b26",
+                },
+                close_button_selected = {
+                    bg = "#1a1b26",
+                },
+
+            }
+        })
+        vim.keymap.set('n', '<leader>nb', ':BufferLinePickClose<CR>', { desc = "Close Buffer" })
+        vim.keymap.set('n', '<S-l>', ':BufferLineCycleNext<CR>', { desc = "Next" })
+        vim.keymap.set('n', '<S-h>', ':BufferLineCyclePrev<CR>', { desc = "Prev" })
+    end
+}
