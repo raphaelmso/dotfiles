8a92152bb35a9cd82d7c899fcbcc2a4e994863d4
raphaelmso <raphael.msoliveira@gmail.com>
2025-01-24 17:37:32 -0300

    change inlay hint color

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -4,6 +4,7 @@ return {
         config = function()
             require("tokyonight").setup()
             vim.cmd([[colorscheme tokyonight-night]])
+            vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "#9ece6a", bg = "#24283b" })
         end
     }
 }
