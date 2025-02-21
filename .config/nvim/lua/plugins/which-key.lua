2089858614a566ac0edac569f8cb959ed6faf375
raphaelmso <raphael.msoliveira@gmail.com>
2025-02-21 19:11:24 -0300

    change keymap

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,6 +1,7 @@
 return {
     "folke/which-key.nvim",
     event = "VeryLazy",
+    delendencies = { "folke/noice.nvim" },
     keys = {
         {
             "<leader>?",
@@ -23,13 +24,14 @@ return {
         vim.keymap.set("n", "<leader>c", "", { desc = "Code" })
         vim.keymap.set("n", "<leader>d", "", { desc = "Debug" })
         vim.keymap.set("n", "<leader>E", "", { desc = "Yazi" })
-        vim.keymap.set("n", "<leader>F", "", { desc = "Find" })
+        vim.keymap.set("n", "<leader>f", "", { desc = "Find" })
         vim.keymap.set("n", "<leader>g", "", { desc = "Git" })
         vim.keymap.set("n", "<leader>gf", "", { desc = "Find" })
         vim.keymap.set("n", "<leader>gd", "", { desc = "Diff" })
         vim.keymap.set("n", "<leader>n", "", { desc = "General" })
         vim.keymap.set("n", "<leader>nc", "", { desc = "Colorizer" })
         vim.keymap.set("n", "<leader>s", "", { desc = "Split" })
+        vim.keymap.set("n", "<leader>t", "", { desc = "ToDo" })
         vim.keymap.set("n", "<leader>X", "", { desc = "Diagnostics" })
         vim.keymap.set("n", "<leader>z", "", { desc = "Surround" })
     end,
